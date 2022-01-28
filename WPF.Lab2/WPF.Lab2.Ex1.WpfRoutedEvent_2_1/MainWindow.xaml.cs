78298a5df0d083f205e2f352560239acad00fcb3
Администратор <@WIN-GMAKK9MSC73>
2022-01-28 15:04:14 +0300

    WPF.Lab2

--- WPF.Lab2/WPF.Lab2.Ex1.WpfRoutedEvent_2_1/MainWindow.xaml.cs
+++ WPF.Lab2/WPF.Lab2.Ex1.WpfRoutedEvent_2_1/MainWindow.xaml.cs
@@ -13,32 +13,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF.Lab2.Ex1.WpfRoutedEvent_2_1
+namespace ITMO.CSCourse.WPF.Practice02_Ex01.WpfRoutedEvent_2_1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        private void textBox1_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-        textBox1_TextChanged_1:
-            MessageBox.Show("Event by textbox");
-            e.Handled = (bool)radBut1.IsChecked;
+		private void Window_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show("Event by Window");
+			e.Handled = (bool)radBut3.IsChecked;
+		}
 
-        Grid_TextChanged_1:
-            MessageBox.Show("Event by Grid");
-            e.Handled = (bool)radBut2.IsChecked;
+		private void Grid_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show("Event by Grid");
+			e.Handled = (bool)radBut2.IsChecked;
+		}
 
-        Window_TextChanged_1:
-            MessageBox.Show("Event by Window");
-            e.Handled = (bool)radBut3.IsChecked;
-        }
-    }
+		private void TextBoxBase_TextChanged_1(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show("Event by textbox");
+			e.Handled = (bool)radBut1.IsChecked;
+		}
+	}
 }
+
+
+
