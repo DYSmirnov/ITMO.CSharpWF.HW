7dc4d3548be3e3efc2f51c85af72014fd7426ad1
Администратор <@WIN-GMAKK9MSC73>
2022-02-01 16:55:30 +0300

    Lab9

---
+++ WPF.Lab9/Lab9.Ex1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace Lab9.Ex1
+{
+
+
+        public class Student
+        {
+            public string StudentName { get; set; }
+            public bool IsEnrolled { get; set; }
+        }
+    }
+
